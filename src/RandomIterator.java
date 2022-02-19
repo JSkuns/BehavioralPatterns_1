260b95c71824e8723bfd5fedfbbca05ca10b0361
Skuns <48394979+JSkuns@users.noreply.github.com>
2022-02-19 20:29:48 +0300

    The job is done

---
+++ src/RandomIterator.java
@@ -0,0 +1,18 @@
+import java.util.Iterator;
+
+public class RandomIterator extends Randoms implements Iterator<Integer> {
+
+    public RandomIterator(int min, int max) {
+        super(min, max);
+    }
+
+    @Override
+    public boolean hasNext() {
+        return true;
+    }
+
+    @Override
+    public Integer next() {
+        return (int) (Math.random() * ((max - min) + 1)) + min;
+    }
+}
